4153903eb76f7290653a6da1cb4401ac2d662dd6
Soeren Domroes <sdo@informatik.uni-kiel.de>
2019-03-26 14:45:18 +0100

    railsl.product: Added railsl perspective.

--- plugins/de.cau.cs.kieler.railsl.product/src/de/cau/cs/kieler/railsl/product/Perspective.java
+++ plugins/de.cau.cs.kieler.railsl.product/src/de/cau/cs/kieler/railsl/product/Perspective.java
@@ -1,5 +1,6 @@
 package de.cau.cs.kieler.railsl.product;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -7,7 +8,23 @@ public class Perspective implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		layout.setEditorAreaVisible(false);
+        // Bottom Row - Left: Project Explorer, Right: Console
+        // Top Row - Editor
+	    
+
+        String editor = layout.getEditorArea();
+        IFolderLayout bottomLeft =
+                layout.createFolder("bottomLeft", IPageLayout.BOTTOM, PerspectiveHelper.BIG, editor);
+
+        IFolderLayout bottomRight =
+                layout.createFolder("bottomRight", IPageLayout.RIGHT, PerspectiveHelper.SMALL,
+                        "bottomLeft");
+
+        PerspectiveHelper.addViewIfExists(bottomRight, PerspectiveHelper.CONSOLE_ID);
+        PerspectiveHelper.addViewIfExists(bottomLeft, PerspectiveHelper.PACKAGE_EXPLORER_ID);
+
+        // Activate editor
+        layout.setEditorAreaVisible(true);
 		layout.setFixed(true);
 	}
 
